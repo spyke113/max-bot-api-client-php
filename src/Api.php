affe904bed4db6b47f1b98493a1499ef5fdb9051
Alex <alex@bushlanov.dev>
2026-03-07 09:47:01 +0300

    tag 1.5.1

--- src/Api.php
+++ src/Api.php
@@ -48,7 +48,7 @@ use RuntimeException;
  */
 class Api
 {
-    public const string LIBRARY_VERSION = '1.5.0';
+    public const string LIBRARY_VERSION = '1.5.1';
 
     public const string API_VERSION = '1.2.5';
 
